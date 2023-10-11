c89e347a1da912f2a4a803ad70530c306d6f1faf
RickCraco <riccardocracolici43@gmail.com>
2023-10-11 15:31:26 +0200

    aggiunte classi stile a fizzbuzz

--- js/script.js
+++ js/script.js
@@ -6,24 +6,26 @@ for(let i = 1; i < 101; i++){
     let box = document.createElement('div');
     wrapper.append(box);
     wrapper.classList.add('d-flex', 'flex-wrap');
-    box.classList.add('box','border', 'border-dark', 'd-flex', 'justify-content-center', 'align-items-center');
+    box.classList.add('box','border','d-flex', 'justify-content-center', 'align-items-center');
     let boxIndex = i;
     box.innerHTML = boxIndex;
 
     if(boxIndex % 3 === 0 && boxIndex % 5 === 0){
         const fizzBuzz = 'FizzBuzz';
         box.innerHTML = fizzBuzz;
-        box.classList.add()
+        box.classList.add('bg-danger')
         console.log(boxIndex);
         console.log(fizzBuzz);
     }else if(boxIndex % 3 === 0){
         const fizz = 'Fizz';
         box.innerHTML = fizz;
+        box.classList.add('bg-info')
         console.log(boxIndex);
         console.log(fizz);
     }else if(boxIndex % 5 === 0){
         const buzz = 'Buzz';
         box.innerHTML = buzz;
+        box.classList.add('bg-success')
         console.log(boxIndex);
         console.log(buzz);
     }
